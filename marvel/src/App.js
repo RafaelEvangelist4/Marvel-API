1547f98c3b309367b616960fa18cb8fe1856459d
Francisco Rafael Evangelista <65771842+RafaelEvangelist4@users.noreply.github.com>
2021-05-29 12:28:59 -0300

    retornando os objetos
    Testando se retorna os objetos da lista contendo os characters

--- marvel/src/App.js
+++ marvel/src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
 import Header from "./components/Header"
+import axios from "axios"
+import React , {useEffect,useState} from "react"
+
+const hash = "8dfe45761350544a26b0837e033c03fb"
 
 function App() {
+  const[items,setItems] = useState([])
+  const[isLoading,setLoading] = useState(true)
+
+  useEffect(()=>{
+    const fetch = async()=>{
+      const result = await axios(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=1e06e4a444f5a1990886ddbd0a78ac84&hash=${hash}`)
+      console.log(result.data)
+      setItems(result.data)
+      setLoading(false)
+    }
+
+    fetch()
+  },[])
+
   return (
     <div className="container">
         <Header />
